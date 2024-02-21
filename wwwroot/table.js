0901d8bcfd4ee870da8aa9dd8e3f775b96e528b7
Zhong Wu <wuzhong1980@gmail.com>
2024-02-21 15:17:06 -0500

    polish code

--- wwwroot/table.js
+++ wwwroot/table.js
@@ -2,84 +2,94 @@ const TABLE_TABS = {
     'PROJECTS': {
         'REQUEST_URL': '/api/admin/projects',
         'TAB_NAME': 'PROJECTS',
-        'DATA_TYPE': 'hub',
-        'BY_DEFAULT': true
+        'CATEGORY_NAME': 'hub',
+        'CATEGORY_DEFAULT': true
     },
     'PROJECT': {
         'REQUEST_URL': '/api/admin/project',
         'TAB_NAME': 'PROJECT',
-        'DATA_TYPE': 'project',
-        'BY_DEFAULT': true
+        'CATEGORY_NAME': 'project',
+        'CATEGORY_DEFAULT': true
     },
     'USERS': {
         'REQUEST_URL': '/api/admin/project/users',
         'TAB_NAME': 'USERS',
-        'DATA_TYPE': 'project',
-        'BY_DEFAULT': false
+        'CATEGORY_NAME': 'project',
+        'CATEGORY_DEFAULT': false
     }
 }
 
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Table class wraps the specific data info
-export class Table {
-    constructor(tableId, accountId = null, projectId = null, tabKey = 'PROJECTS', dataSet = null) {
-        this.tableId = tableId;
-        this.accountId = accountId;
-        this.projectId = projectId;
-        this.tabKey = tabKey;
-        this.dataSet = dataSet;
+class Table {
+    #tableId;
+    #accountId;
+    #projectId;
+    #tabKey;
+    #dataSet;
+
+    constructor(tableId, accountId = null, projectId = null, tabKey = 'PROJECTS') {
+        this.#tableId = tableId;
+        this.#accountId = accountId;
+        this.#projectId = projectId;
+        this.#tabKey = tabKey;
+        this.#dataSet = null;
     };
 
+    get tabKey(){
+        return this.#tabKey;
+    }
+
     resetDataInfo( tabKey=null, accountId=null, projectId=null ){
-        this.tabKey = tabKey? tabKey: this.tabKey;
-        this.accountId = accountId? accountId: this.accountId;
-        this.projectId = accountId||projectId? projectId: this.projectId;
-        this.dataSet = null;
+        this.#tabKey = tabKey? tabKey: this.#tabKey;
+        this.#accountId = accountId? accountId: this.#accountId;
+        this.#projectId = accountId||projectId? projectId: this.#projectId;
+        this.#dataSet = null;
     }
 
     async prepareDataAndDraw(){
-        const url = TABLE_TABS[this.tabKey].REQUEST_URL;
+        const url = TABLE_TABS[this.#tabKey].REQUEST_URL;
         const data = {
-            'accountId': this.accountId,
-            'projectId': this.projectId
+            'accountId': this.#accountId,
+            'projectId': this.#projectId
         }
         try {
             const response = await axios.get(url, { params: data } );
-            this.dataSet = response.data;
+            this.#dataSet = response.data;
         } catch (err) {
             console.error(err);
             return;
         }
-        for (var key in this.dataSet[0]) {
-            if (Array.isArray(this.dataSet[0][key]) || typeof this.dataSet[0][key] === 'object' && this.dataSet[0][key] != null) {
-                this.dataSet.forEach(item => {
+        // Mark "N/A" for complicated properties.
+        for (var key in this.#dataSet[0]) {
+            if (Array.isArray(this.#dataSet[0][key]) || typeof this.#dataSet[0][key] === 'object' && this.#dataSet[0][key] != null) {
+                this.#dataSet.forEach(item => {
                     item[key] = "N/A";
                 })
             }
         }
         let columns = [];
-        for (var key in this.dataSet[0]) {
+        for (var key in this.#dataSet[0]) {
             columns.push({
                 field: key,
                 title: key,
                 align: "center"
             })
         }
-        $(this.tableId).bootstrapTable('destroy');
-        $(this.tableId).bootstrapTable({
-            data: this.dataSet,
+        $(this.#tableId).bootstrapTable('destroy');
+        $(this.#tableId).bootstrapTable({
+            data: this.#dataSet,
             customToolbarButtons: [
                 {
                     name: "grid-export",
                     title: "Export",
-                    icon: "glyphicon-import",
+                    icon: "glyphicon-export",
                     callback: exportData
                 },
                 {
                     name: "grid-import",
                     title: "Import",
-                    icon: "glyphicon-export",
+                    icon: "glyphicon-import",
                     callback: importData
                 }
             ],
@@ -102,61 +112,65 @@ export class Table {
     }
 
     exportToCSV() {
-        if (this.dataSet == null || this.dataSet.length == 0) {
+        if (this.#dataSet == null || this.#dataSet.length == 0) {
             console.warn('DataSet is not ready, please fetch your data first.');
             return;
         }
-
-        let csvData = [];
+        let csvDataList = [];
         let csvHeader = [];
-
-        for (let key in this.dataSet[0]) {
+        for (let key in this.#dataSet[0]) {
             csvHeader.push(key);
         }
-        csvData.push(csvHeader);
-        this.dataSet.forEach((row) => {
-            let csvRowTmp = [];
+        csvDataList.push(csvHeader);
+        this.#dataSet.forEach((row) => {
+            let csvRowItem = [];
             for (let key in row) {
                 if (typeof row[key] === 'string')
-                    csvRowTmp.push("\"" + row[key].replace(/\"/g, "\"\"").replace("#", "") + "\"")
+                    csvRowItem.push("\"" + row[key].replace(/\"/g, "\"\"").replace("#", "") + "\"")
                 else
-                    csvRowTmp.push(row[key]);
+                    csvRowItem.push(row[key]);
             }
-            csvData.push(csvRowTmp);
+            csvDataList.push(csvRowItem);
         })
-        let csvString = csvData.join("%0A");
+        let csvString = csvDataList.join("%0A");
         let a = document.createElement('a');
         a.href = 'data:attachment/csv,' + csvString;
         a.target = '_blank';
-        a.download = this.tabKey + (new Date()).getTime() + '.csv';
+        a.download = this.#tabKey + (new Date()).getTime() + '.csv';
         document.body.appendChild(a);
         a.click();
     }
 
-    async importFromCSV( csvInputData ){
-        if( csvInputData == null ){
+    async importFromCSV( inputCSVData ){
+        if( inputCSVData == null ){
             console.warn('The input csv file is not ready, please provide correct csv data.');
             return;
         }
-        const rows = csvInputData.split("\r\n");
+        const rows = inputCSVData.split("\r\n");
         const keys = rows[0].split(',');
-        let csvData = [];
+        let requestDataList = [];
         for (let i = 1; i < rows.length; i++) {
-            let jsonData = {};
-            var cells = rows[i].split(",");
-            for (var j = 0; j < cells.length; j++) {
+            let jsonDataItem = {};
+            let cells = rows[i].split(",");
+            for (let j = 0; j < cells.length; j++) {
                 if (cells[j] == null || cells[j] == '')
                     continue
+                // customize the input property
                 let key = keys[j];
-                switch (this.tabKey) {
+                switch (this.#tabKey) {
                     case 'PROJECTS':
                         if (key == 'template') {
-                            jsonData[key] = { 'projectId': cells[j] };
+                            jsonDataItem[key] = { 'projectId': cells[j] };
                             continue;
                         }
                         break;
                     case 'PROJECT':
                     case 'USERS':
+                        if(key == 'roleIds'){
+                            const roleIdList = cells[j].replace(/\s/g,'').split('|');
+                            jsonDataItem[key] = roleIdList;
+                            continue;
+                        }
                         const params = key.split('.')
                         const length = params.length;
                         if (length == 2 && params[0] == 'products') {
@@ -164,10 +178,10 @@ export class Table {
                                 "key": params[length - 1],
                                 "access": cells[j]
                             }
-                            if (jsonData["products"] == null) {
-                                jsonData["products"] = [];
+                            if (jsonDataItem["products"] == null) {
+                                jsonDataItem["products"] = [];
                             }
-                            jsonData["products"].push(productAccess)
+                            jsonDataItem["products"].push(productAccess)
                             continue
                         }
                         break;
@@ -175,16 +189,16 @@ export class Table {
                         console.warn("The current Admin Data Type is not expected");
                         break;
                 }
-                jsonData[key] = cells[j];
+                jsonDataItem[key] = cells[j];
             }
-            csvData.push(jsonData);
+            requestDataList.push(jsonDataItem);
         }
         const data = {
-            'accountId': this.accountId,
-            'projectId': this.projectId,
-            'data': csvData
+            'accountId': this.#accountId,
+            'projectId': this.#projectId,
+            'data': requestDataList
         }
-        const url = TABLE_TABS[this.tabKey].REQUEST_URL;
+        const url = TABLE_TABS[this.#tabKey].REQUEST_URL;
         let response = null;
         try {
             response = await axios.post(url, data );
@@ -195,7 +209,6 @@ export class Table {
     }
 }
 
-
 function exportData() {
     if (!g_accDataTable ) {
         alert('The CSV data is not ready, please generate the data first.')
@@ -203,7 +216,6 @@ function exportData() {
     }
     g_accDataTable.exportToCSV();
 }
-
 
 function importData() {
     let input = document.createElement('input');
@@ -215,16 +227,12 @@ function importData() {
             if (typeof (FileReader) != "undefined") {
                 var reader = new FileReader();
                 reader.onload = async function (e) {
-                    if (!g_accDataTable) {
-                        alert('please select one collection!');
-                        return;
-                    }
                     showTable(false);
                     try {
                         await g_accDataTable.importFromCSV(e.target.result);
                         await g_accDataTable.prepareDataAndDraw();
                     } catch (err) {
-                        console.log(err);
+                        console.warn(err);
                     }
                     showTable(true);
                 }
@@ -240,33 +248,38 @@ function importData() {
 }
 
 function showTable( visible ){
-    $('#table_area')[0].hidden = !visible;
+    $('#tableArea')[0].hidden = !visible;
     $('#workingAnimation')[0].hidden = visible;
 }
 
 export async function refreshTable( accountId = null, projectId=null ) {
     showTable(false);
-
-    if (projectId == null) {
-        $("#PROJECTS").addClass("active");
-        $("#PROJECTS").removeClass("hidden")
-
-        $("#PROJECT").removeClass("active");
-        $("#PROJECT").addClass("hidden")
-
-        $("#USERS").removeClass("active");
-        $("#USERS").addClass("hidden")
-
-    } else {
-        $("#PROJECTS").removeClass("active");
-        $("#PROJECTS").addClass("hidden")
-
-        $("#PROJECT").addClass("active");
-        $("#PROJECT").removeClass("hidden")
-
-        $("#USERS").removeClass("active");
-        $("#USERS").removeClass("hidden")
-    } 
+    if( TABLE_TABS[g_accDataTable.tabKey].CATEGORY_NAME=='hub' && projectId ){
+        for (let key in TABLE_TABS) {
+            if( TABLE_TABS[key].CATEGORY_NAME == 'hub' ){
+                $("#" + key).addClass("hidden");
+                $("#" + key).removeClass("active");
+            }
+            else{
+                if( TABLE_TABS[key].CATEGORY_DEFAULT )
+                    $("#" + key).addClass("active");
+                $("#" + key).removeClass("hidden");
+            }
+        } 
+    }
+    if (TABLE_TABS[g_accDataTable.tabKey].CATEGORY_NAME == 'project' && !projectId) {
+        for (let key in TABLE_TABS) {
+            if (TABLE_TABS[key].CATEGORY_NAME == 'hub') {
+                $("#" + key).removeClass("hidden");
+                if (TABLE_TABS[key].CATEGORY_DEFAULT)
+                    $("#" + key).addClass("active");
+            }
+            else {
+                $("#" + key).addClass("hidden");
+                $("#" + key).removeClass("active");
+            }
+        }
+    }
     const activeTab = $("ul#adminTableTabs li.active")[0].id;
     g_accDataTable.resetDataInfo( activeTab, accountId, projectId );
     await g_accDataTable.prepareDataAndDraw();
@@ -276,26 +289,21 @@ export async function refreshTable( accountId = null, projectId=null ) {
 export async function initTableTabs(){
     // add all tabs
     for (let key in TABLE_TABS) {
-        $('<li id=' + key + '><a href="acc_table" data-toggle="tab">' + TABLE_TABS[key].TAB_NAME + '</a></li>').appendTo('#adminTableTabs');
-        $("#" + key).addClass((TABLE_TABS[key].DATA_TYPE == 'hub' && TABLE_TABS[key].BY_DEFAULT) ? "active" : "hidden");
-        $("#" + key).removeClass((TABLE_TABS[key].DATA_TYPE == 'hub' && TABLE_TABS[key].BY_DEFAULT)? "hidden" : "active");
+        $('<li id=' + key + '><a href="accTable" data-toggle="tab">' + TABLE_TABS[key].TAB_NAME + '</a></li>').appendTo('#adminTableTabs');
+        $("#" + key).addClass((TABLE_TABS[key].CATEGORY_NAME == 'hub' && TABLE_TABS[key].CATEGORY_DEFAULT) ? "active" : "hidden");
     } 
     // event on the tabs
     $('a[data-toggle="tab"]').on('shown.bs.tab', async function (e) {
-        if (g_accDataTable == null) {
-            console.warn("The table is not ready, please create the table first!");
-            return;
-        }
         showTable(false);        
         try {
             const activeTab = e.target.parentElement.id;
             g_accDataTable.resetDataInfo(activeTab);
             await g_accDataTable.prepareDataAndDraw();
         } catch (err) {
-            console.log(err);
+            console.warn(err);
         }    
         showTable(true);
     });  
 }
 
-var g_accDataTable = new Table('#acc_table' );
+var g_accDataTable = new Table('#accTable' );
